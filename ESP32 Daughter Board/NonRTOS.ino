d124041567610a655d3da5f6d9b091092f320ff9
Locksley3D <39237700+Locksley3D@users.noreply.github.com>
2024-08-29 23:59:30 +0300

    Update NonRTOS.ino

--- ESP32 Daughter Board/NonRTOS.ino
+++ ESP32 Daughter Board/NonRTOS.ino
@@ -14,6 +14,8 @@ const int gate7pin = 27;
 const int pumpPin = 19;
 const int ledPin = 18;
 
+bool pumpState = false;
+
 String receivedCommand;
 int recieved_status = 0;
 int commands[8];
@@ -50,15 +52,15 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(gate6pin), gate6Intr, RISING);
   attachInterrupt(digitalPinToInterrupt(gate7pin), gate7Intr, RISING);
 
-  Wire.begin();
-  sensor.setTimeout(500);
-  if (!sensor.init())
-  {
-    Serial.println("Failed to detect and initialize sensor!");
-    while (1) {}
-  }
+  // Wire.begin();
+  // sensor.setTimeout(500);
+  // if (!sensor.init())
+  // {
+  //   Serial.println("Failed to detect and initialize sensor!");
+  //   while (1) {}
+  // }
 
-  sensor.startContinuous();
+  // sensor.startContinuous();
 
 }
 
@@ -68,9 +70,10 @@ void loop() {
     if(recieved_status==1){
       switch (commands[0]){
         case 1:
-          Serial.println("Got data");
+          // Serial.println("Got data");
           togglePumps(commands[1]);
           break;
+          
       }
       recieved_status=0;
     }
@@ -94,15 +97,15 @@ void loop() {
     current_gate=0;
   }
 
-  tofValue=sensor.readRangeContinuousMillimeters();
-  // Serial.print("2,");
-  // Serial.println(reading);
-  if(tofValue<170){
-    digitalWrite(ledPin,HIGH);
-  }else{
-    digitalWrite(ledPin,LOW);
-  }
-  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+  // tofValue=sensor.readRangeContinuousMillimeters();
+  // // Serial.print("2,");
+  // // Serial.println(reading);
+  // if(tofValue<170){
+  //   digitalWrite(ledPin,HIGH);
+  // }else{
+  //   digitalWrite(ledPin,LOW);
+  // }
+  // if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
   // Serial.println();
 
   delay(20);
@@ -145,14 +148,30 @@ void gate7Intr() {
 
 void togglePumps(int mode){
   if(mode==1){
+    pumpState=true;
     digitalWrite(pumpPin,HIGH);
     digitalWrite(ledPin,HIGH);
-    Serial.println("2,Turning pumps on");
+    // Serial.println("2,Turning pumps on");
     }
   else if(mode==0){
+    pumpState=false;
     digitalWrite(pumpPin,LOW);
     digitalWrite(ledPin,LOW);
-    Serial.println("2,Turning pumps off");
+    // Serial.println("2,Turning pumps off");
+  }
+  else if(mode==2){
+    if(pumpState==false){
+      pumpState=true;
+      digitalWrite(pumpPin,HIGH);
+      digitalWrite(ledPin,HIGH);
+      // Serial.println("2,Turning pumps on");
+    }
+    else if(pumpState==true){
+      pumpState=false;
+      digitalWrite(pumpPin,LOW);
+      digitalWrite(ledPin,LOW);
+      // Serial.println("2,Turning pumps off");
+    }
   }
 }
 
